0d20bd7313608386f2cc7206ecbd75689409cb08
Anshul patle <patleanshul920@gmail.com>
2023-03-18 23:57:49 +0530

    Update App.js

--- game-of-thrones/src/App.js
+++ game-of-thrones/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './Component/Header';
+import Characters from './Component/Characters';
 
 function App() {
   return (
     <div className="App">
      <Header/>
+     <Characters/>
     </div>
   );
 }
