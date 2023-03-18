50de62c732b615dae9ee73e61618488b2125f74c
Anshul patle <patleanshul920@gmail.com>
2023-03-19 01:09:38 +0530

    Update Character.jsx

--- game-of-thrones/src/Component/Character.jsx
+++ game-of-thrones/src/Component/Character.jsx
@@ -1,19 +1,40 @@
 import React from 'react'
-import { Card, styled } from '@mui/material';
+import { Box, Card, styled , Typography} from '@mui/material';
 
 const Image = styled('img')({
     width: '100%',
     height: 300
 })
 
+const Component = styled(Card)`
+width: 200px;
+margin: 10px;
+position: relative;
+`
+const Container = styled(Box)`
+position: absolute;
+top: 0;
+color: #fff;
+height: 300px;
+width:100%;
+background: #000;
+opacity: 0.5;
+`
 
 const Character = ({value}) => {
     console.log(value)
   return (
     value.characterImageFull?
-        <Card>
+        <Component>
             <Image src={value.characterImageFull} alt="" />
-        </Card>
+            <Container>   
+                <Typography>Name: {value.characterName}</Typography>
+                <Typography>Real Name: {value.actorName}</Typography>
+                <Typography>Real House : {value.houseName}</Typography>
+                <Typography>Real Nickname : {value.nickname}</Typography>
+
+            </Container>
+        </Component>
         :''
   )
 }
